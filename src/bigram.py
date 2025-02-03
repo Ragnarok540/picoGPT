b78f9e881c229b11c7925ed64d735f6f05b0ec84
Edgar Nova <ragnarok540@gmail.com>
2025-02-02 19:21:41 -0500

    bigram

---
+++ src/bigram.py
@@ -0,0 +1,82 @@
+from input import Input
+from codec import Codec
+from loader import Loader
+import torch
+import torch.nn as nn
+from torch.nn import functional as F
+
+
+class BigramLanguageModel(nn.Module):
+
+    def __init__(self, vocab_size):
+        super().__init__()
+        self.token_embedding_table = nn.Embedding(vocab_size, vocab_size)
+
+    def forward(self, idx, targets=None):
+        logits = self.token_embedding_table(idx)
+
+        if targets is None:
+            loss = None
+        else:
+            B, T, C = logits.shape
+            logits = logits.view(B*T, C)
+            targets = targets.view(B*T)
+            loss = F.cross_entropy(logits, targets)
+
+        return logits, loss
+
+    def generate(self, idx, max_new_tokens):
+        for _ in range(max_new_tokens):
+            logits, loss = self(idx)
+            logits = logits[:, -1, :]
+            probs = F.softmax(logits, dim=-1)
+            idx_next = torch.multinomial(probs, num_samples=1)
+            idx = torch.cat((idx, idx_next), dim=1)
+        return idx
+
+
+if __name__ == '__main__':
+    eval_iters = 200
+    learning_rate = 1e-2
+    eval_interval = 300
+    max_iters = 3000
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    torch.manual_seed(1337)
+
+    input = Input('input.txt')
+    codec = Codec(input.chars)
+    loader = Loader(input, codec)
+
+    @torch.no_grad()
+    def estimate_loss():
+        out = {}
+        model.eval()
+        for split in ['train', 'val']:
+            losses = torch.zeros(eval_iters)
+            for k in range(eval_iters):
+                X, Y = loader.get_batch(split)
+                logits, loss = model(X, Y)
+                losses[k] = loss.item()
+            out[split] = losses.mean()
+        model.train()
+        return out
+
+    model = BigramLanguageModel(input.vocab_size)
+    m = model.to(device)
+
+    optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate)
+
+    for iter in range(max_iters):
+        if iter % eval_interval == 0:
+            losses = estimate_loss()
+            print(f"step {iter}: train loss {losses['train']:.4f}, val loss {losses['val']:.4f}")
+
+        xb, yb = loader.get_batch('train')
+
+        logits, loss = model(xb, yb)
+        optimizer.zero_grad(set_to_none=True)
+        loss.backward()
+        optimizer.step()
+
+    context = torch.zeros((1, 1), dtype=torch.long, device=device)
+    print(codec.decode(m.generate(context, max_new_tokens=500)[0].tolist()))
